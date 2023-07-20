3c6277abdc7e04158f73c7699f340941b137f7c1
HALRINSKEY <hal4554yoko@gmail.com>
2023-07-20 14:18:26 +0900

    Update notice_bot_2.0.py

--- notice_bot_2.0.py
+++ notice_bot_2.0.py
@@ -1,6 +1,8 @@
 import discord
 from tracemalloc import start
 import time
+from discord import app_commands
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,19 +15,18 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
         # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        self.botRoom = client.get_channel(966831929058025482) 
+        self.botRoom = client.get_channel() 
 
 #noticebot------------------------------------------------------------------------------
-    async def on_voice_state_update(self, member, before, after):    
+    async def on_voice_state_update(self, member, before, after):
         
+        # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
+        announceChannelIds = []
+        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+        botRoom = client.get_channel()
 
         # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
         if before.channel != after.channel:
-
- 
-            # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
-            announceChannelIds = [966831929058025483]
- 
 
             # 入室通知
             if after.channel is not None and after.channel.id in announceChannelIds:
@@ -35,35 +36,48 @@ class MyClient(discord.Client):
                     t = time.time() - self.leave_time
 
                     if t < 5:
-                        pass
+                        pass                       
                     else:
-                        await self.botRoom.send(member.name + " entered in " + after.channel.name)
+                        await botRoom.send(member.name + " entered in " + after.channel.name)
        
                 else:
-                    await self.botRoom.send(member.name + " entered in " + after.channel.name)
+                    await botRoom.send(member.name + " entered in " + after.channel.name)
 
-        #退室
-        if before.channel is not None and before.channel.id in announceChannelIds:
+            #退室処理
+            if before.channel is not None and before.channel.id in announceChannelIds:
             
-            self.leave_name = member.name
-            self.leave_time = time.time()
+                self.leave_name = member.name
+                self.leave_time = time.time()
 #-----------------------------------------------------------------------------------------
 
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
-
-        if message.content.startswith("/hello"):
-            await message.channel.send("hello")
-
-        if message.content.startswith("/channel"):  #channel id
-            channels = {}
-            for ch in client.get_all_channels():
-                channels[ch.name] = str(ch.id)
-
-            for name, id in channels.items():
-                await message.channel.send("name:" + name + "    id:" + id)
-
-
 client = MyClient(intents=intents)
+
+#command----------------------------------------------------------------------------------
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+@client.tree.command()
+async def getserverid(interaction: discord.Interaction):
+   """serverID"""
+   serverId = interaction.guild.id
+   await interaction.response.send_message(serverId)
+
+@client.tree.command()
+async def channel(interaction: discord.Interaction):
+    """chanelID"""
+    channels = {}
+    for ch in client.get_all_channels():
+        channels[ch.name] = str(ch.id)
+
+    #console output
+    #for name, id in channels.items():
+    #    print("name:" + name + "    id:" + id)
+
+    ch_id ="\n".join(["name:{0}      id:{1}".format(key, value) for (key, value) in channels.items()])
+    await interaction.response.send_message(ch_id)
+
+#------------------------------------------------------------------------------------------
+
 client.run("token")
